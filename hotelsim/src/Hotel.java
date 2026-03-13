1950a0932e66436ee655d73707a86b7f1ce7e87a
mahmoudali2027 <23168080@student.hhs.nl>
2026-03-13 15:26:36 +0100

    Hotel en Main klasse aangepast

--- hotelsim/src/Hotel.java
+++ hotelsim/src/Hotel.java
@@ -1,14 +1,33 @@
+import java.util.List;
 public class Hotel {
+    //breedte en hoogte van het hotel
     int breedte;
     int hoogte;
-    //layout vakje;
-    //arraylist ruimte ruimtes;
-    //arraylist persoon perssonen;
 
+    Layout layout;
+
+    // lijst van alle ruimtes binnen het hotel
+    List <Ruimte> ruimtes;
+
+    //lijst van alle personnen gast en schoonmakers
+    List <Persoon> personen;
+
+// lift en trap in het hotel
+    Lift lift;
+    Trap trap;
     //constructor
     public Hotel(){}
+     // laadt het layout van het hotel uit een bestand
+    public void laadLayoutBestand(){
 
-    public void laadLayoutBestand(){} //parameter bestand
-    public void voegPersoonToe(){} //parameter Persoon p
-    public void krijgRuimteOp(){} //parameter x,y
+    }
+    // voegt een persoon toe aan het hotel
+    public void voegPersoonToe(Persoon p){
+
+    }
+    // geeft de ruimte terug op positie
+    public Ruimte krijgRuimteOp(int x, int y) {
+        return null; //TODO: implementatie later toevoegen
+    }
 }
+
--- hotelsim/src/Main.java
+++ hotelsim/src/Main.java
@@ -1,4 +1,8 @@
 //main file
-void main() {
+public class Main {
+    public static void main (String[] args){
+
+        System.out.println ("Hotel simulation started");
+    }
     
 }
