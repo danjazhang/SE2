ab551aaac5b8b3d89d264d49627e1731cb47b3ad
yaskenture <25080512@student.hhs.nl>
2026-03-17 11:53:25 +0100

    ik heb een grid renderer gemaakt die het hotel tekent op het scherm

--- hotelsim/src/HotelPanel.java
+++ hotelsim/src/HotelPanel.java
@@ -1,9 +1,42 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class HotelPanel extends JPanel {
     Hotel hotel;
 
-    public HotelPanel(Hotel hotel) {}
+    //pixelgrootte per vakje
+    static int tileSize = 64;
+
+    public HotelPanel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){
+        //tekent de achtergrond leeg, altijd eerst aanroepen
+        super.paintComponent(g);
+
+        //loop over elk vakje in het grid
+        for(int x = 1; x <= hotel.breedte; x++){
+            for (int y = 1; y <= hotel.hoogte; y++){
+                Ruimte r = hotel.krijgRuimteOp(x, y);
+
+                //kies kleur op basis van ruimtetype
+                if (r instanceof Kamer) g.setColor(Color.BLUE);
+                else if (r instanceof Restaurant) g.setColor(Color.ORANGE);
+                else if (r instanceof Bioscoop) g.setColor(Color.RED);
+                else if (r instanceof Fitnesruimte) g.setColor(Color.GREEN);
+                else g.setColor(Color.LIGHT_GRAY);
+
+                //teken gevulde rechthoek op de juiste pixelpositie
+                g.fillRect((x-1)* tileSize, (y-1)*tileSize, tileSize, tileSize);
+
+                //teken zwarte rand eromheen
+                g.setColor(Color.BLACK);
+                g.drawRect((x-1)* tileSize, (y-1)*tileSize, tileSize, tileSize);
 
 
+            }
+        }
+    }
 }
--- hotelsim/src/Main.java
+++ hotelsim/src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Hotel hotel = new Hotel();
 
+
         // pas het pad aan naar waar jouw JSON file staat
         hotel.laadLayoutBestand("layout.json");
+
+        new HotelFrame(hotel);
 
         // test: print alle ruimtes
         for (Ruimte r : hotel.ruimtes) {
