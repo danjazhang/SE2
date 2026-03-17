050baf4f9bd90a7100a996ea04224ac9012136e7
delisha11 <delishaashruf@gmail.com>
2026-03-17 12:38:14 +0100

    layout, vakje en hotel bijgewerkt. lobby en lift ook bijgewerkt

--- hotelsim/src/Lift.java
+++ hotelsim/src/Lift.java
@@ -1,18 +1,48 @@
-public class Lift {
-    int huidigeverdieping;
-    //Richitng Richting;
-    //arraylist persoon passagiers
-    //verdiepingverzoeken list?
+import java.util.ArrayList;
+import java.util.List;
 
-    //constructor
-    public Lift(){
-        this.huidigeverdieping = 0;
+public class Lift {
+
+    private int huidigeVerdieping;
+    private List<Persoon> passagiers;
+    private List<Integer> verzoeken;
+    private boolean deurOpen;
+
+    // constructor
+    public Lift() {
+        this.huidigeVerdieping = 0;
+        this.passagiers = new ArrayList<>();
+        this.verzoeken = new ArrayList<>();
+        this.deurOpen = false;
     }
 
-    public void gaOmhoog(){}
-    public void gaOmlaag(){}
-    public void openDeur(){}
-    public void sluitDeur(){}
-    public void voegVerzoekToe(){}
+    public void gaOmhoog() {
+        huidigeVerdieping++;
+    }
 
+    public void gaOmlaag() {
+        huidigeVerdieping--;
+    }
+
+    public void openDeur() {
+        deurOpen = true;
+    }
+
+    public void sluitDeur() {
+        deurOpen = false;
+    }
+
+    public void voegVerzoekToe(int verdieping) {
+        if (!verzoeken.contains(verdieping)) {
+            verzoeken.add(verdieping);
+        }
+    }
+
+    public int getHuidigeVerdieping() {
+        return huidigeVerdieping;
+    }
+
+    public List<Integer> getVerzoeken() {
+        return verzoeken;
+    }
 }
--- hotelsim/src/Lobby.java
+++ hotelsim/src/Lobby.java
@@ -1,11 +1,28 @@
-public class Lobby extends Ruimte{
-    Layout balielocatie;
+public class Lobby extends Ruimte {
 
-    //constructor
-    public Lobby(Layout balielocatie){
-        this.balielocatie = balielocatie;
+    private int balieX;
+    private int balieY;
+
+    // constructor
+    public Lobby(int posX, int posY, int breedte, int hoogte, int balieX, int balieY) {
+        super(posX, posY, breedte, hoogte);
+        this.balieX = balieX;
+        this.balieY = balieY;
     }
 
-    public void toonStatusScherm(){}
-    public void pauzeerSim(){}
+    public void toonStatusScherm() {
+        System.out.println("Status van hotel wordt getoond...");
+    }
+
+    public void pauzeerSim() {
+        System.out.println("Simulatie gepauzeerd.");
+    }
+
+    public int getBalieX() {
+        return balieX;
+    }
+
+    public int getBalieY() {
+        return balieY;
+    }
 }
