d4d87ac29bb97a2cccc484d2c3bd9bdbcbd33ab6
yaskenture <25080512@student.hhs.nl>
2026-03-17 12:34:19 +0100

    ik heb arraylists gemaakt in de klassen die dat nodig hebben

--- hotelsim/src/Scenario.java
+++ hotelsim/src/Scenario.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scenario {
     // arraylist gebeurtenis gebeurtenissen
+    List<Gebeurtenis> gebeurtenissen;
 
     //constructor
-    public Scenario(){}
+    public Scenario(){
+        this.gebeurtenissen = new ArrayList<>();
+    }
 
     public void krijgGebeurtenissen(){} //parameter tijd
     public void voegGebeurtenisToe(){} //parameter Gebeurtenis g
