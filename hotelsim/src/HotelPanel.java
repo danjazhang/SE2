ce1ad16641a27e0244ed21e86b25f5e1f28efa69
yaskenture <25080512@student.hhs.nl>
2026-03-24 12:01:42 +0100

    ik heb elk vakje een duidelijke weergave met naam gegeven

--- hotelsim/src/HotelPanel.java
+++ hotelsim/src/HotelPanel.java
@@ -22,10 +22,11 @@ public class HotelPanel extends JPanel {
                 Ruimte r = hotel.krijgRuimteOp(x, y);
 
                 //kies kleur op basis van ruimtetype
-                if (r instanceof Kamer) g.setColor(Color.BLUE);
+                if (r instanceof Kamer) g.setColor(new Color(70, 130, 180)); //rgb kleur
                 else if (r instanceof Restaurant) g.setColor(Color.ORANGE);
                 else if (r instanceof Bioscoop) g.setColor(Color.RED);
                 else if (r instanceof Fitnesruimte) g.setColor(Color.GREEN);
+                else if (r instanceof Lobby) g.setColor(Color.YELLOW);
                 else g.setColor(Color.LIGHT_GRAY);
 
                 //teken gevulde rechthoek op de juiste pixelpositie
@@ -34,6 +35,17 @@ public class HotelPanel extends JPanel {
                 //teken zwarte rand eromheen
                 g.setColor(Color.BLACK);
                 g.drawRect((x-1)* tileSize, (y-1)*tileSize, tileSize, tileSize);
+
+                //naam tekenen
+                String naam;
+                if (r != null){
+                    naam = r.getClass().getSimpleName();
+                }else{
+                    naam = "";
+                }
+                g.setColor(Color.BLACK);
+                g.setFont(new Font("Arial", Font.BOLD,12));
+                g.drawString(naam, (x-1) * tileSize +4, (y-1)* tileSize+16);
 
 
             }
