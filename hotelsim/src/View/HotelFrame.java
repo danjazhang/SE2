bbca54dd192ffe4f2d1e79f939aa0d0b2bace685
delisha11 <delishaashruf@gmail.com>
2026-03-31 17:39:49 +0200

    pauze en stop knop bijgevoed  bij nieuwe code UI

--- hotelsim/src/View/HotelFrame.java
+++ hotelsim/src/View/HotelFrame.java
@@ -45,6 +45,8 @@ public class HotelFrame extends JFrame {
         JButton importButton = new JButton("Import layout");
         layoutSelector = new JComboBox<>();
         JButton startButton = new JButton("Start simulatie");
+        JButton pauseButton = new JButton("Pauzeer");
+        JButton stopButton = new JButton("Stop");
 
         // button om een hotel layout bestand te importeren
         importButton.addActionListener(e -> {
@@ -100,11 +102,29 @@ public class HotelFrame extends JFrame {
             manager.start(1);
         });
 
+        // PAUZE
+        pauseButton.addActionListener(e -> {
+            manager.pauze();
+
+            if (pauseButton.getText().equals("Pauze")) {
+                pauseButton.setText("Resume");
+            } else {
+                pauseButton.setText("Pauze");
+            }
+        });
+
+        // STOP
+        stopButton.addActionListener(e -> {
+            manager.stop();
+        });
+
         // bovenste balk met knoppen en dropdown
         JPanel top = new JPanel();
         top.add(importButton);
         top.add(layoutSelector);
         top.add(startButton);
+        top.add(pauseButton);
+        top.add(stopButton);
         add(top, BorderLayout.NORTH);
 
         // hoofdweergave met scroll mogelijkheid voor grotere layouts
