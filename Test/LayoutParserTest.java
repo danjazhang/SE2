b7e42b23d9ba111486d0b6e3d062311f3549835d
nuseema <25077716@student.hhs.nl>
2026-04-12 17:17:20 +0200

    unit testen van kamer,layouparser en ruimte aangepast

--- Test/LayoutParserTest.java
+++ Test/LayoutParserTest.java
@@ -4,55 +4,49 @@ import Model.*;
 
 public class LayoutParserTest {
 
-    // na het laden moet het hotel de juiste breedte en hoogte hebben
+    // na het laden moet het resultaat niet null zijn
+    @Test
+    void testLaadGeeftResultaatTerug() {
+        LayoutParser parser = new LayoutParser();
+        ParseResultaat resultaat = parser.laad("layout.json");
+        assertNotNull(resultaat);
+    }
+
+    // na het laden moet het resultaat de juiste breedte en hoogte hebben
     @Test
     void testLaadVultBreedteEnHoogte() {
-        Hotel hotel = new Hotel();
         LayoutParser parser = new LayoutParser();
-        parser.laad("layout.json", hotel);
+        ParseResultaat resultaat = parser.laad("layout.json");
         // layout.json heeft een hotel van 6 breed en 8 hoog
-        assertEquals(6, hotel.breedte);
-        assertEquals(8, hotel.hoogte);
+        assertEquals(6, resultaat.breedte);
+        assertEquals(8, resultaat.hoogte);
     }
 
-    // na het laden moet het hotel de juiste hoeveelheid ruimtes hebben
+    // na het laden moet de ruimteData lijst gevuld zijn
     @Test
-    void testLaadVultRuimtes() {
-        Hotel hotel = new Hotel();
+    void testLaadVultRuimteData() {
         LayoutParser parser = new LayoutParser();
-        parser.laad("layout.json", hotel);
+        ParseResultaat resultaat = parser.laad("layout.json");
         // layout.json heeft 29 ruimtes
-        assertEquals(29, hotel.ruimtes.size());
+        assertEquals(29, resultaat.ruimteData.size());
     }
 
-    // na het laden moet de layout niet null zijn
+    // bij een ongeldig bestandspad moet laad() null teruggeven en niet crashen
     @Test
-    void testLaadMaaktLayoutAan() {
-        Hotel hotel = new Hotel();
+    void testLaadMetOngeldigPadGeeftNull() {
         LayoutParser parser = new LayoutParser();
-        parser.laad("layout.json", hotel);
-        // layout moet aangemaakt zijn
-        assertNotNull(hotel.layout);
+        // ongeldig pad moet null teruggeven en niet crashen
+        ParseResultaat resultaat = parser.laad("bestaat_niet.json");
+        assertNull(resultaat);
     }
 
-    // bij een ongeldig bestandspad moet laad() false teruggeven en niet crashen
+    // elk json object in ruimteData moet een _posX veld hebben
     @Test
-    void testLaadMetOngeldigPadGeeftFalse() {
-        Hotel hotel = new Hotel();
+    void testLaadVoegdPositieToe() {
         LayoutParser parser = new LayoutParser();
-        // ongeldig pad moet false teruggeven en niet crashen
-        boolean resultaat = parser.laad("bestaat_niet.json", hotel);
-        assertFalse(resultaat);
-    }
-
-    // na het laden moet de eerste ruimte op positie (1,1) een Kamer zijn
-    @Test
-    void testLaadMaaktJuisteRuimteTypes() {
-        Hotel hotel = new Hotel();
-        LayoutParser parser = new LayoutParser();
-        parser.laad("layout.json", hotel);
-        // positie (1,1) moet een Kamer zijn
-        assertNotNull(hotel.krijgRuimteOp(1, 1));
-        assertTrue(hotel.krijgRuimteOp(1, 1) instanceof Kamer);
+        ParseResultaat resultaat = parser.laad("layout.json");
+        // elk object moet een _posX veld hebben dat door de parser is toegevoegd
+        assertTrue(resultaat.ruimteData.get(0).has("_posX"));
+        assertTrue(resultaat.ruimteData.get(0).has("_posY"));
     }
 }
